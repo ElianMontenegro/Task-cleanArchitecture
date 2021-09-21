2ee8746a90977505319d7cee09ad8b0dfeb7d76e
unknown <elianmontenegro491@gmail.com>
2021-09-20 23:42:57 -0300

    refactor: create makeSut factory

--- tests/presentation/controllers/signup-controller.spec.ts
+++ tests/presentation/controllers/signup-controller.spec.ts
@@ -1,9 +1,28 @@
 import { IController, IHttpRequest, IHttpResponse } from '@/../../src/presentation/protocols'
 import { badRequest } from '@/../../src/presentation/helpers'
+import faker, { fake } from 'faker'
+
+
+const makeSut = () => {
+    const mockRequest = {
+        body: {
+            username : faker.name.findName(),
+            email: faker.internet.email(),
+            password : faker.internet.password(),
+            passwordConfirmation: faker.internet.password()
+        }
+    }
+    const sut = new SignUpController()
+    return {
+        mockRequest,
+        sut
+    }
+}
+
 export class SignUpController implements IController{
     async handle(request: IHttpRequest): Promise<IHttpResponse>{
         try {
-            const paramsRequired = ["username", "email", "password", "paswordConfirm"]
+            const paramsRequired = ["username", "email", "password", "passwordConfirmation"]
             for (const params of paramsRequired) {
                 if (!request.body[params]) {
                     return badRequest(params)
@@ -26,49 +45,27 @@ export class SignUpController implements IController{
 
 describe("SignUpController", () => {
     test("Should return badRequest Error if username is not provided",async () => {
-        const sut = new SignUpController()
-        const httpRequest = {
-            body : {
-                username: ""
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest);
+        const { sut, mockRequest } = makeSut()
+        mockRequest.body.username = ""
+        const httpResponse = await sut.handle(mockRequest);
         expect(httpResponse).toEqual(badRequest("username"))
     })
     test("Should return badRequest Error if email is not provided",async () => {
-        const sut = new SignUpController()
-        const httpRequest = {
-            body : {
-                username: "any_username",
-                email: ""
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest);
+        const { sut, mockRequest } = makeSut()
+        mockRequest.body.email = ""
+        const httpResponse = await sut.handle(mockRequest);
         expect(httpResponse).toEqual(badRequest("email"))
     })
     test("Should return badRequest Error if password is not provided",async () => {
-        const sut = new SignUpController()
-        const httpRequest = {
-            body : {
-                username: "any_username",
-                email: "any_email",
-                password: ""
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest);
+        const { sut, mockRequest } = makeSut()
+        mockRequest.body.password = ""
+        const httpResponse = await sut.handle(mockRequest);
         expect(httpResponse).toEqual(badRequest("password"))
     })
-    test("Should return badRequest Error if paswordConfirm is not provided",async () => {
-        const sut = new SignUpController()
-        const httpRequest = {
-            body : {
-                username: "any_username",
-                email: "any_email",
-                password: "any_password",
-                paswordConfirm: ""
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest);
-        expect(httpResponse).toEqual(badRequest("paswordConfirm"))
+    test("Should return badRequest Error if passwordConfirmation is not provided",async () => {
+        const { sut, mockRequest } = makeSut()
+        mockRequest.body.passwordConfirmation = ""
+        const httpResponse = await sut.handle(mockRequest);
+        expect(httpResponse).toEqual(badRequest("passwordConfirmation"))
     })
 })

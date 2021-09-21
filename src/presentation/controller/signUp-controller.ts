c97d41da9bcf1e094f092be36d678df2fdc83208
unknown <elianmontenegro491@gmail.com>
2021-09-21 00:28:58 -0300

    test: ensure signUpController return 500 if AddAccount throws

--- src/presentation/controller/signUp-controller.ts
+++ src/presentation/controller/signUp-controller.ts
@@ -1,7 +1,10 @@
 import { IController, IHttpRequest, IHttpResponse } from '@/../../src/presentation/protocols'
-import { badRequest } from '@/../../src/presentation/helpers'
+import { badRequest, forbidden, serverError } from '@/../../src/presentation/helpers'
+import { AddAccount } from '../../domain/usecases'
+import { EmailInUseError } from '../errors'
 
 export class SignUpController implements IController{
+    constructor(private readonly addAccount : AddAccount){}
     async handle(request: IHttpRequest): Promise<IHttpResponse>{
         try {
             const paramsRequired = ["username", "email", "password", "passwordConfirmation"]
@@ -10,14 +13,25 @@ export class SignUpController implements IController{
                     return badRequest(params)
                 }
             }
+            const { username, email, password } = request.body
+            const isValid = await this.addAccount.add({
+                username,
+                email,
+                password
+            })
+            
+            if (!isValid) {
+                return forbidden(new EmailInUseError())
+            }
+
             return {
                 statusCode: 200,
                 body: ""
             }
-        } catch (error) {
+        } catch (error: any) {
             return {
                 statusCode: 500,
-                body: error
+                body: serverError(error)
             }
         }
     }

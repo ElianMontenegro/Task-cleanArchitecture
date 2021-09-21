a81e70739b9dcf3b72352175218088d37c138e9b
unknown <elianmontenegro491@gmail.com>
2021-09-20 22:43:23 -0300

    feat: create http-helpers

---
+++ src/presentation/errors/index.ts
@@ -0,0 +1,2 @@
+export * from '../errors/unauthorized-error'
+export * from '../errors/server-error'
---
+++ src/presentation/helpers/http-helpers.ts
@@ -0,0 +1,32 @@
+import { IHttpResponse } from '../protocols'
+import { ServerError, UnauthorizedError } from '../errors'
+
+export const badRequest = (error: Error): IHttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): IHttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const unauthorized = (): IHttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const serverError = (error: Error): IHttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack!)
+})
+
+export const ok = (data: any): IHttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): IHttpResponse => ({
+  statusCode: 204,
+  body: null
+})

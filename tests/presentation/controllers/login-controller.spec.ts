b19ca6f37c45bca7b29ff581d674d4758288ce4a
unknown <elianmontenegro491@gmail.com>
2021-09-23 18:58:48 -0300

    test: add makeSut factory return loginController, mockRequest

--- tests/presentation/controllers/login-controller.spec.ts
+++ tests/presentation/controllers/login-controller.spec.ts
@@ -3,30 +3,32 @@ import { MissingParamError } from "../../../src/presentation/errors";
 import { badRequest } from "../../../src/presentation/helpers";
 import { LoginController } from '@/../../src/presentation/controller'
 
-
+const makeSut = () => {
+    const mockRequest = {
+        body: {
+            email: faker.internet.email(),
+            password :  faker.internet.password(),
+        }
+    }
+    const sut = new LoginController();
+    return {
+        sut,
+        mockRequest
+    }
+}
 
 
 describe('LoginController ', () => {
     test('Should return badRequest Error if email is not provided', async () => {
-        const sut = new LoginController();
-        const httpRequest = {
-            body: {
-                email : "",
-                password : ""
-            }
-        }
-        const auth = await sut.handle(httpRequest)
+        const { sut, mockRequest } = makeSut();
+        mockRequest.body.email = ""
+        const auth = await sut.handle(mockRequest)
         expect(auth).toEqual(badRequest(new MissingParamError("email")))
     })
     test('Should return badRequest Error if password is not provided', async () => {
-        const sut = new LoginController();
-        const httpRequest = {
-            body: {
-                email : faker.internet.email(),
-                password : ""
-            }
-        }
-        const auth = await sut.handle(httpRequest)
+        const { sut, mockRequest } = makeSut();
+        mockRequest.body.password = ""
+        const auth = await sut.handle(mockRequest)
         expect(auth).toEqual(badRequest(new MissingParamError("password")))
     })
 })

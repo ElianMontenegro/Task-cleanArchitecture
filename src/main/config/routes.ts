a95541874ac387ca71e334de69b8adcb037d2c4b
unknown <elianmontenegro491@gmail.com>
2021-09-24 19:10:59 -0300

    chore: config import routes to routes

---
+++ src/main/config/routes.ts
@@ -0,0 +1,12 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+
+export default (app: Express) => {
+    const router = Router()
+    app.use('/api', router)
+    readdirSync(join(__dirname, '../routes')).map(async file => {
+        (await import (`../routes/${file}`)).default(router)
+    })
+}

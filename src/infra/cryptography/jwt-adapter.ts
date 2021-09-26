69c896e73628cb43d473d03187ab2b133d953b29
unknown <elianmontenegro491@gmail.com>
2021-09-25 22:22:55 -0300

    feat: add express-route-adapter to adapters

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -9,7 +9,7 @@ export class JwtAdapter implements AccessToken, RefreshToken {
     }
 
     async refreshToken (id: string, email : string, secret : string, expiresIn : any): Promise<string>{
-        const token = jwt.sign({ id : id }, secret , { expiresIn: expiresIn });
+        const token = jwt.sign({ id : id, email: email}, secret , { expiresIn: expiresIn });
         return token
     }
     

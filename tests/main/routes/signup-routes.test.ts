52f66c5d38eee3cdafff3f1f30b6894c611cdec9
unknown <elianmontenegro491@gmail.com>
2021-09-27 10:55:21 -0300

    test: ensure /api/signup return 403 if email already exists

--- tests/main/routes/signup-routes.test.ts
+++ tests/main/routes/signup-routes.test.ts
@@ -6,6 +6,15 @@ import faker, { fake } from 'faker'
 
 let accountCollection : Collection
 
+const password = faker.internet.password()
+const user = {
+    username : faker.name.findName(),
+    email : faker.internet.email(),
+    password : password,
+    passwordConfirmation : password
+}
+
+
 describe('Login routes', () => {
     beforeAll(async () => {
         await mongoHelper.connect(process.env.MONGO_URL)
@@ -22,16 +31,20 @@ describe('Login routes', () => {
 
     describe('signup route', () => {
         test('Should return 200 on signup', async () => {
-            const password = faker.internet.password()
             await request(app)
                 .post('/api/signup')
-                .send({
-                    username : faker.name.findName(),
-                    email : faker.internet.email(),
-                    password : password,
-                    passwordConfirmation : password
-                })
+                .expect("Content-Type", /json/)
+                .send(user)
                 .expect(200)
+        })
+
+        test('Should return 403 if email already exists', async () => {
+            await accountCollection.insertOne(user)
+            await request(app)
+                .post('/api/signup')
+                .expect("Content-Type", /json/)
+                .send(user)
+                .expect(403)
         })
     })
 })

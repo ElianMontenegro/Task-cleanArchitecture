a8aae33bd07b02bca99fc5e49970641aad189b54
unknown <elianmontenegro491@gmail.com>
2021-09-27 10:19:27 -0300

    chore: config server express

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express'
 import middleware from './middleware'
 import routes from './routes'
+import { config as dotenv } from 'dotenv'
 const app : Express = express() 
 
 middleware(app)
 routes(app)
+dotenv()
 export default app;

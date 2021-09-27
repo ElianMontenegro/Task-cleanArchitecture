d3176867276d156f2d8efa0483aa6c3a47d78bb1
unknown <elianmontenegro491@gmail.com>
2021-09-27 11:15:03 -0300

    test: ensure /api/login return 200 on login

--- tests/main/routes/signup-routes.test.ts
+++ tests/main/routes/signup-routes.test.ts
@@ -2,7 +2,9 @@ import { mongoHelper } from '../../../src/infra/db/mongodb'
 import request from 'supertest'
 import app from '../../../src/main/config/app'
 import { Collection } from 'mongodb'
-import faker, { fake } from 'faker'
+import faker from 'faker'
+import bcrypt from 'bcrypt'
+
 
 let accountCollection : Collection
 
@@ -45,6 +47,20 @@ describe('Login routes', () => {
                 .expect("Content-Type", /json/)
                 .send(user)
                 .expect(403)
+        })
+    })
+
+    describe('login route', () => {
+        
+        test('Should return 200 on login', async () => {
+            const hashPassword = await bcrypt.hash("12345", 10)
+            user.password = hashPassword
+            await accountCollection.insertOne(user)
+            await request(app)
+                .post('/api/login')
+                .expect('Content-Type', /json/)
+                .send({ email : user.email, password : "12345" })
+                .expect(200)
         })
     })
 })

39ea2a74e84b6ff84380e102620fc7d1f06357c0
unknown <elianmontenegro491@gmail.com>
2021-09-29 18:57:17 -0300

    test: move sut and httpRequest creation a factory helper methods

--- tests/presentation/controllers/add-task-controller.spec.ts
+++ tests/presentation/controllers/add-task-controller.spec.ts
@@ -1,28 +1,34 @@
-
 import { MissingParamError } from "../../../src/presentation/errors"
 import { badRequest } from "../../../src/presentation/helpers"
 import { AddTaskController } from '../../../src/presentation/controller'
+import faker from 'faker'
 
+const makeSut = () => {
+    const httpRequest = {
+        body : {
+            title : faker.name.title(),
+            content : faker.lorem.words()
+        }
+    }
+    const sut = new AddTaskController()
+    return {
+        sut,
+        httpRequest
+    }
+}
 
 
 describe('AddTaskController', () => {
     test('Should return error if title is not provided', async () =>{
-        const sut = new AddTaskController()
-        const httpRequest = {
-            body : {title : ""}
-        }
+        const { sut, httpRequest } = makeSut()
+        httpRequest.body.title = ""       
         const res = await sut.handle(httpRequest)
         expect(res).toEqual(badRequest(new MissingParamError('title')))
     })
 
     test('Should return error if content is not provided', async () =>{
-        const sut = new AddTaskController()
-        const httpRequest = {
-            body : {
-                title : "any_title",
-                content : ""
-            }
-        }
+        const { sut, httpRequest } = makeSut()
+        httpRequest.body.content = ""
         const res = await sut.handle(httpRequest)
         expect(res).toEqual(badRequest(new MissingParamError('content')))
     })

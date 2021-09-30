da15fd34bf051f9bba31340474f431ef5c3f7a50
unknown <elianmontenegro491@gmail.com>
2021-09-29 23:18:04 -0300

    refactor: add inject dependecy loadAccountByToken to AuthMiddleware

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,2 +1,3 @@
 export * from "./add-account"
 export * from "./authetication"
+export * from "./load-account-by-token"
--- src/presentation/middleware/auth-middleware.ts
+++ src/presentation/middleware/auth-middleware.ts
@@ -1,17 +1,21 @@
 import { AccessDeniedError } from "../errors"
 import { forbidden, serverError } from "../helpers"
 import { IHttpRequest, IHttpResponse, Middleware } from "../protocols"
-
+import { LoadAccountByToken } from '../../domain/usecases'
 
 
 export class AuthMiddleware implements Middleware{
+
+    constructor(private readonly loadAccountByToken : LoadAccountByToken){}
+
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse>{
 
         try {
             const { accessToken } = httpRequest.body
-            if(!accessToken){
-                return forbidden(new AccessDeniedError())
+            if(accessToken){
+                const account = await this.loadAccountByToken.load(accessToken)
             }
+            return forbidden(new AccessDeniedError())
         
         } catch (error) {
             return {

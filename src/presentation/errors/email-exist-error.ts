3d54f7e4d4e2701502d68895c942b6f598cec57c
unknown <elianmontenegro491@gmail.com>
2021-10-02 17:32:58 -0300

    refactor: change EmailInuseError name and performs

--- src/presentation/errors/email-exist-error.ts
+++
@@ -1,6 +0,0 @@
-export class EmailInUseError extends Error {
-    constructor () {
-      super('EmailInUseError: The received email is already in use')
-      this.name = 'EmailInUseError: The received email is already in use'
-    }
-  }

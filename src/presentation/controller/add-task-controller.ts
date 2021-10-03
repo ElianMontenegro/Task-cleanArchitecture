7ca8f4616c4d9198e78ed31892ba0ba62f9a3bbe
unknown <elianmontenegro491@gmail.com>
2021-10-02 22:47:16 -0300

    refactor: add addTass usecase

--- src/presentation/controller/add-task-controller.ts
+++ src/presentation/controller/add-task-controller.ts
@@ -1,21 +1,33 @@
 import { MissingParamError, DataInUseError } from "../errors"
-import { badRequest } from "../helpers"
+import { badRequest, ok, serverError } from "../helpers"
 import { IController, IHttpRequest, IHttpResponse } from "../protocols"
-import { CheckTaskByTitle } from '../../data/protocols/db'
+import { CheckTaskByTitle, AddTask } from '../../data/protocols/db'
+
 
 export class AddTaskController implements IController {
-    constructor (private readonly checkTaskByTitle : CheckTaskByTitle) {}
+    constructor (
+        private readonly checkTaskByTitle : CheckTaskByTitle,
+        private readonly addTask : AddTask
+
+    ) {}
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse>{
-        const { title, content } = httpRequest.body
-        const paramsRequired = ["title", "content", "accountId"]
-        for (const params of paramsRequired) {
-            if(!httpRequest.body[params]){
-                return badRequest(new MissingParamError(params))
+        try {
+            const { title, content } = httpRequest.body
+            const accountId = httpRequest.accountId
+            const paramsRequired = ["title", "content", "accountId"]
+            for (const params of paramsRequired) {
+                if(!httpRequest.body[params]){
+                    return badRequest(new MissingParamError(params))
+                }
             }
-        }
-        const exist = await this.checkTaskByTitle.checkByTitle(title)
-        if(exist){
-            return badRequest(new DataInUseError('title'))
+            const exist = await this.checkTaskByTitle.checkByTitle(title)
+            if(exist){
+                return badRequest(new DataInUseError('title'))
+            }
+            const task = await this.addTask.add({ title, content, accountId })
+            return ok(task)
+        } catch (error: any) {
+            return serverError(error)
         }
     }
     

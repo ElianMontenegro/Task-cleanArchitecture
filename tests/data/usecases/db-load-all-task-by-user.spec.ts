bdb4c55089c1f7eb0bb448e6aa06229dbbdcbfe9
unknown <elianmontenegro491@gmail.com>
2021-10-07 23:56:57 -0300

    test: ensure throw error loadAllTaskByUserRepositorySpy throw error

--- tests/data/usecases/db-load-all-task-by-user.spec.ts
+++ tests/data/usecases/db-load-all-task-by-user.spec.ts
@@ -1,6 +1,8 @@
 import { DbLoadAllTaskByUser } from '../../../src/data/usecases'
 import { mockLoadAllTaskResult } from '../../domain/mocks'
 import { LoadAllTaskByUserRepositorySpy } from '../mocks'
+import { throwError } from '../../presentation/mocks'
+
 import faker from 'faker'
 const makeSut = () => {
     const id = faker.datatype.uuid()
@@ -22,11 +24,18 @@ describe('LoadAllTaskByUser use cases', () => {
         expect(tasks).toBeNull()
     })
 
-    test('Should return a tasks if LoadAllTaskRepositorySpy return a tasks', async () => {
+    test('Should return a tasks if loadAllTaskByUserRepositorySpy return a tasks', async () => {
         const { sut, loadAllTaskByUserRepositorySpy, id } = makeSut()
         const loadAllTaskResult = mockLoadAllTaskResult()
         loadAllTaskByUserRepositorySpy.result = loadAllTaskResult
         const tasks = await sut.loadByUser(id)
         expect(tasks).toEqual(loadAllTaskResult)
+    })
+
+    test('Should throw error loadAllTaskByUserRepositorySpy throw error', async () => {
+        const { sut, loadAllTaskByUserRepositorySpy, id } = makeSut()
+        jest.spyOn(loadAllTaskByUserRepositorySpy, 'loadAllTaksByUser').mockImplementationOnce(throwError)
+        const tasks = sut.loadByUser(id)
+        await expect(tasks).rejects.toThrowError()
     })
 })

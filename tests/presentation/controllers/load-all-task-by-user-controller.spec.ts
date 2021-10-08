d599d1fd43dbd49ac741109f7395618743b7cab3
unknown <elianmontenegro491@gmail.com>
2021-10-07 23:10:17 -0300

    test: ensure LoadAllTaskByUserController call with correct value

--- tests/presentation/controllers/load-all-task-by-user-controller.spec.ts
+++ tests/presentation/controllers/load-all-task-by-user-controller.spec.ts
@@ -1,6 +1,7 @@
 import { LoadAllTaskByUserController } from '../../../src/presentation/controller'
 import { notFound } from '../../../src/presentation/helpers'
 import { LoadAllTaskByUserSpy } from '../mocks' 
+import faker from 'faker'
 
 const makeSut = () => {
     const loadAllTaskByUserSpy = new LoadAllTaskByUserSpy()
@@ -17,5 +18,12 @@ describe('LoadAllTaskByUserController', () => {
         loadAllTaskByUserSpy.tasks = null
         const httpResponse = await sut.handle({accountId : ''})
         expect(httpResponse).toEqual(notFound(new Error('there are not tasks')))
+    })
+
+    test('Should call loadAllTaskByUser with correct values', async () => {
+        const { sut, loadAllTaskByUserSpy} = makeSut()
+        const httpRequest = {accountId : faker.datatype.uuid()}
+        await sut.handle(httpRequest)
+        expect(loadAllTaskByUserSpy.accountId).toEqual(httpRequest.accountId)
     })
 })

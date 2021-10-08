f49bd745932259e78832441017e2a12841012680
unknown <elianmontenegro491@gmail.com>
2021-10-08 18:50:48 -0300

    feat: add /load-all-task-by-user to tasks routes

--- src/main/routes/task-routes.ts
+++ src/main/routes/task-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { AdaptRoute } from "../adapters/express-route-adapter";
-import { makeAddTaskController, makeLoadAllTaskController } from "../factories";
+import { makeAddTaskController, makeLoadAllTaskController, makeLoadAllTaskByUserController } from "../factories";
 import { auth } from "../middlewares";
 
 
 export default (router : Router): void => {
     router.post('/add-task', auth, AdaptRoute(makeAddTaskController()))
     router.get('/load-all-task', AdaptRoute(makeLoadAllTaskController()))
+    router.get('/load-all-task-by-user', auth, AdaptRoute(makeLoadAllTaskByUserController()))
 }

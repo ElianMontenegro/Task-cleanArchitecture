ee159135aa90eb8fa39076bd20f13648fb5374fd
unknown <elianmontenegro491@gmail.com>
2021-10-07 23:54:37 -0300

    test: ensure  return a tasks if loadAllTaskByUserRepositorySpy return a tasks

--- tests/data/usecases/db-load-all-task-by-user.spec.ts
+++ tests/data/usecases/db-load-all-task-by-user.spec.ts
@@ -1,21 +1,32 @@
 import { DbLoadAllTaskByUser } from '../../../src/data/usecases'
+import { mockLoadAllTaskResult } from '../../domain/mocks'
 import { LoadAllTaskByUserRepositorySpy } from '../mocks'
-
+import faker from 'faker'
 const makeSut = () => {
+    const id = faker.datatype.uuid()
     const loadAllTaskByUserRepositorySpy = new LoadAllTaskByUserRepositorySpy()
     const sut = new DbLoadAllTaskByUser(loadAllTaskByUserRepositorySpy)
     return {
         sut, 
-        loadAllTaskByUserRepositorySpy
+        loadAllTaskByUserRepositorySpy,
+        id
     }
 }
 
 
 describe('LoadAllTaskByUser use cases', () => {
-    test('Should return null if loadAllTaskByUserRepositorySpy return null ', async () => {
-        const { sut, loadAllTaskByUserRepositorySpy } = makeSut()
+    test('Should return null if loadAllTaskByUserRepositorySpy return null', async () => {
+        const { sut, loadAllTaskByUserRepositorySpy, id } = makeSut()
         loadAllTaskByUserRepositorySpy.result = null
-        const tasks = await sut.loadByUser('')
+        const tasks = await sut.loadByUser(id)
         expect(tasks).toBeNull()
+    })
+
+    test('Should return a tasks if LoadAllTaskRepositorySpy return a tasks', async () => {
+        const { sut, loadAllTaskByUserRepositorySpy, id } = makeSut()
+        const loadAllTaskResult = mockLoadAllTaskResult()
+        loadAllTaskByUserRepositorySpy.result = loadAllTaskResult
+        const tasks = await sut.loadByUser(id)
+        expect(tasks).toEqual(loadAllTaskResult)
     })
 })
